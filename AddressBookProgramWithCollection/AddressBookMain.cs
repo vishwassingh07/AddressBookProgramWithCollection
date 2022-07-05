0a15a3bec29b9d4106509566ae747c24eee96b3e
Vishwas <baghel.vishwas0@gmail.com>
2022-07-05 14:29:50 +0530

    [Vishwas]add: Editing Contact In Address Book

--- AddressBookProgramWithCollection/AddressBookMain.cs
+++ AddressBookProgramWithCollection/AddressBookMain.cs
@@ -9,11 +9,42 @@ namespace AddressBookProgramWithCollection
     public class AddressBookMain
     {
         //Create dictionary to store result
-        Dictionary<string, string> Contacts = new Dictionary<string, string>();
+        Dictionary<string, string> Contacts;
+        Dictionary<string, Dictionary<string, string>> AddressBook = new Dictionary<string, Dictionary<string, string>>();
 
         //UC1 - Create Contact
-        public void CreateContact()
+        //public void CreateContact()
+        //{
+        //    Console.Write("First Name : ");
+        //    Contacts.Add("First Name ", Console.ReadLine());
+
+        //    Console.Write("Last Name : ");
+        //    Contacts.Add("Last Name ", Console.ReadLine());
+
+        //    Console.Write("Address : ");
+        //    Contacts.Add("Address ", Console.ReadLine());
+
+        //    Console.Write("City : ");
+        //    Contacts.Add("City ", Console.ReadLine());
+
+        //    Console.Write("State : ");
+        //    Contacts.Add("State ", Console.ReadLine());
+
+        //    Console.Write("Zip Code : ");
+        //    Contacts.Add("Zip Code ", Console.ReadLine());
+
+        //    Console.Write("Phone Number : ");
+        //    Contacts.Add("Phone Number  ", Console.ReadLine());
+
+        //    Console.Write("Email Address : ");
+        //    Contacts.Add("Email Address ", Console.ReadLine());
+        //}
+        //Adding Contact To List
+        public void AddContact()
         {
+            Console.WriteLine("Add contact");
+            Contacts = new Dictionary<string, string>();
+
             Console.Write("First Name : ");
             Contacts.Add("First Name ", Console.ReadLine());
 
@@ -38,6 +69,79 @@ namespace AddressBookProgramWithCollection
             Console.Write("Email Address : ");
             Contacts.Add("Email Address ", Console.ReadLine());
 
+            Contacts.TryGetValue("First Name", out string FirstName);
+            Contacts.TryGetValue("Last Name", out string LastName);
+            AddressBook.Add(FirstName + " " + LastName, Contacts);
+            Console.WriteLine("Contact added\n");
+        }
+
+        public void ViewContact()
+        {
+            Console.WriteLine("Enter full name:");
+            string contactName = Console.ReadLine();
+            if (AddressBook.ContainsKey(contactName))
+            {
+                Contacts = new Dictionary<string, string>();
+                AddressBook.TryGetValue(contactName, out Contacts);
+                Console.WriteLine("First Name: " + Contacts["first Name"]);
+
+                Console.WriteLine("Last Name:" + Contacts["last Name"]);
+
+
+            }
+            else
+                Console.WriteLine("Contact doesn't exist");
+
+        }
+        //UC3 - Editing Contact
+        public void EditContact()
+        {
+            //Edit information
+            Console.WriteLine("Enter full contact name");
+            String contactName = Console.ReadLine();
+
+            if (AddressBook.ContainsKey(contactName))
+            {
+                Console.Write("Select option you want to edit : ");
+                Console.WriteLine("1. First Name 2. Last Name 3. Address\n 4. City 5. State 6. Zip\n 7. Phone number  8. Email");
+
+                int option = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter contact field:");
+                String Cotanctinfo = Console.ReadLine();
+
+                switch (option)
+                {
+                    case 1:
+                        AddressBook[contactName]["First Name"] = Cotanctinfo;
+                        break;
+                    case 2:
+                        AddressBook[contactName]["Last Name"] = Cotanctinfo;
+                        break;
+                    case 3:
+                        AddressBook[contactName]["Address"] = Cotanctinfo;
+                        break;
+                    case 4:
+                        AddressBook[contactName]["City"] = Cotanctinfo;
+                        break;
+                    case 5:
+                        AddressBook[contactName]["State"] = Cotanctinfo;
+                        break;
+                    case 6:
+                        AddressBook[contactName]["Zip"] = Cotanctinfo;
+                        break;
+                    case 7:
+                        AddressBook[contactName]["Phone number"] = Cotanctinfo;
+                        break;
+                    case 8:
+                        AddressBook[contactName]["Email"] = Cotanctinfo;
+                        break;
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("contact doesn't exist");
+            }
         }
         //Display Contact
         public void Display()
@@ -49,3 +153,5 @@ namespace AddressBookProgramWithCollection
         }
     }
 }
+
+
--- AddressBookProgramWithCollection/Program.cs
+++ AddressBookProgramWithCollection/Program.cs
@@ -8,26 +8,35 @@ namespace AddressBookProgramWithCollection
             Console.WriteLine("Welcome To The Address Book Program\n===================================");
             AddressBookMain addingContact = new AddressBookMain();
             bool end = true;
-            Console.WriteLine("Choose an option to execute :\n1. Create Contact\n2. Add Contact\n3. End The Program");
-            int option = Convert.ToInt32(Console.ReadLine());
-            switch (option)
+            while (end)
             {
-                case 1:
-                    addingContact.CreateContact();
-                    Console.WriteLine("Contact has created");
-                    break;
-                case 2:
-                    addingContact.CreateContact();
-                    Console.WriteLine("Contact has added ");
-                    addingContact.Display();
-                    break;
-                case 3:
-                    end = false;
-                    break;
-                default:
-                    Console.WriteLine("Invalid option ");
-                    break;
+                Console.WriteLine("Choose an option to execute :\n1. Create Contact\n2. Add Contact\n3. Edit Contact\n4." +
+                    " End The Program");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        addingContact.AddContact();
+                        Console.WriteLine("Contact has created");
+                        break;
+                    case 2:
+                        addingContact.AddContact();
+                        Console.WriteLine("Contact has added ");
+                        addingContact.Display();
+                        break;
+                    case 3:
+                        addingContact.EditContact();
+                        addingContact.ViewContact();
+                        break;
+                    case 4:
+                        end = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option ");
+                        break;
+                }
             }
+
         }
     }
 }
