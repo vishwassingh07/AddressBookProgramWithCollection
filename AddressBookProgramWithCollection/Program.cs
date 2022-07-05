f51e7f28be6561f40afb25b65525a6cd4acfb4c9
Vishwas <baghel.vishwas0@gmail.com>
2022-07-05 12:36:42 +0530

    [Vishwas]add: Address Book Program Added

---
+++ AddressBookProgramWithCollection/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace AddressBookProgramWithCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Address Book Program\n===================================");
+        }
+    }
+}
