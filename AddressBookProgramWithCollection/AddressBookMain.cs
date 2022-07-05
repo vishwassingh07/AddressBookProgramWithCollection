aa6c59a0d910fb043bbc1766bb524ff99e3ff779
Vishwas <baghel.vishwas0@gmail.com>
2022-07-05 12:56:04 +0530

    [Vishwas]add: Creating Contact In Address Book

---
+++ AddressBookProgramWithCollection/AddressBookMain.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProgramWithCollection
+{
+    public class AddressBookMain
+    {
+        //Create dictionary to store result
+        Dictionary<string, string> Contacts = new Dictionary<string, string>();
+
+        //UC1 - Create Contact
+        public void CreateContact()
+        {
+            Console.Write("First Name : ");
+            Contacts.Add("First Name ", Console.ReadLine());
+
+            Console.Write("Last Name : ");
+            Contacts.Add("Last Name ", Console.ReadLine());
+
+            Console.Write("Address : ");
+            Contacts.Add("Address ", Console.ReadLine());
+
+            Console.Write("City : ");
+            Contacts.Add("City ", Console.ReadLine());
+
+            Console.Write("State : ");
+            Contacts.Add("State ", Console.ReadLine());
+
+            Console.Write("Zip Code : ");
+            Contacts.Add("Zip Code ", Console.ReadLine());
+
+            Console.Write("Phone Number : ");
+            Contacts.Add("Phone Number  ", Console.ReadLine());
+
+            Console.Write("Email Address : ");
+            Contacts.Add("Email Address ", Console.ReadLine());
+
+        }
+    }
+}
--- AddressBookProgramWithCollection/Program.cs
+++ AddressBookProgramWithCollection/Program.cs
@@ -6,6 +6,22 @@ namespace AddressBookProgramWithCollection
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To The Address Book Program\n===================================");
+            AddressBookMain addingContact = new AddressBookMain();
+            bool end = true;
+            Console.WriteLine("Choose an option to execute :\n1. Create Contact\n2. End The Program");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    addingContact.CreateContact();
+                    break;
+                case 2:
+                    end = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option ");
+                    break;
+            }
         }
     }
 }
